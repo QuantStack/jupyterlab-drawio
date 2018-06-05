56b302fb9a1717636563ba82f6307c63132ece29
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-06-04 22:32:16 -0400

    add src of javascript

--- scripts/copyfiles.js
+++ scripts/copyfiles.js
@@ -1,4 +1,5 @@
 var fs = require('fs-extra');
+fs.copySync('src/mxgraph/javascript/src/', 'lib/mxgraph/javascript/src/');
 fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/', 'lib/mxgraph/javascript/examples/grapheditor/www/');
 // fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.txt',
 // 			'lib/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.md');
