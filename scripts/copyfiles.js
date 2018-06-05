ec4fe3ae7fbe161c1e3a7420cbd1921b986b59bf
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-06-04 22:05:09 -0400

    Only copy grapheditor

--- scripts/copyfiles.js
+++ scripts/copyfiles.js
@@ -1,5 +1,5 @@
 var fs = require('fs-extra');
-fs.copySync('src/mxgraph/', 'lib/mxgraph/');
+fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/', 'lib/mxgraph/javascript/examples/grapheditor/www/');
 // fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.txt',
 // 			'lib/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.md');
 // fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.txt',
