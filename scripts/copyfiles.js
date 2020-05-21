7a031f8d94979f19c9abe98dd1acf918ef92d0b8
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-05-20 23:28:29 -0400

    move submodule

--- scripts/copyfiles.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs-extra');
-
-fs.copySync('src/mxgraph/javascript/src/', 'lib/mxgraph/javascript/src/');
-fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/', 'lib/mxgraph/javascript/examples/grapheditor/www/');
-// fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.txt',
-// 			'lib/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.md');
-// fs.copySync('src/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.txt',
-// 			'src/mxgraph/javascript/examples/grapheditor/www/resources/grapheditor.md');
